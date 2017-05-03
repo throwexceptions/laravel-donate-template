32e0f57177e3136113c257f13ebbb350e30894f4
Renier Trenuela <throwexceptions@users.noreply.github.com>
2017-05-03 10:09:35 +0800

    Update stripe.php

--- config/stripe.php
+++ config/stripe.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-	'secret_key' => 'sk_test_GewcOvMltPYT3DUJVXIsHvOG',
-	'publishable_key' => 'pk_test_gJmirbBzERZje1CGQsc6wqT9'
+	'secret_key' => '(api key provided)',
+	'publishable_key' => '(api key provided)'
 ];
